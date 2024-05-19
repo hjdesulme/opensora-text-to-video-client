d384693c784857e80131edd2a173bd4aa11b1e70
Ayush <ayushrgarg@gmail.com>
2024-05-18 18:24:50 -0700

    Array Buffer

--- src/pages/api/generateVideo.ts
+++ src/pages/api/generateVideo.ts
@@ -5,11 +5,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { prompt } = req.body;
 
     try {
-      const response = await fetch('http://your-flask-backend-url/generate', {
+      const response = await fetch('http://127.0.0.1:5000/video', {
         method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        // headers: {
+        //   'Content-Type': 'application/json',
+        // },
         body: JSON.stringify({ prompt }),
       });
 
@@ -17,10 +17,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         throw new Error('Failed to generate video');
       }
 
-      const data = await response.json();
-      res.status(200).json({ videoUrl: data.videoUrl });
+      // const data = await response.json();
+      const buffer = await response.arrayBuffer()
+      res.setHeader('Content-Type', 'video/mp4');
+      res.setHeader('Content-Disposition', 'attachment; filename="downloaded_video.mp4"');
+      res.status(200).send(buffer);
+      // res.status(200).json({ videoUrl: data.videoUrl });
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ error: "Error" });
     }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
